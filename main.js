3787066cbeb465fb9e7a96d59d7fb5eb5a8fc222
Ranveer Thind <ranveerthind01@hotmail.com>
2020-05-22 19:25:05 -0700

    Navigation Field

---
+++ main.js
@@ -0,0 +1,11 @@
+function navclose() {
+    var nav2 = document.getElementById('nav2');
+
+    nav2.style.right = '-100%';
+}
+
+function navopen() {
+    var nav2 = document.getElementById('nav2');
+
+    nav2.style.right = '0%';
+}
